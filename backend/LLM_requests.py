0cea4eecd5e55c5c58e514d57430699659b88ec6
Isa Khan <isakhan1000@gmail.com>
2024-05-12 14:06:10 -0400

    Update backend files and requirements.txt

--- backend/LLM_requests.py
+++ backend/LLM_requests.py
@@ -1,60 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
 from langchain_community.llms import OpenAI
 import pandas as pd
-from pyspark.sql import functions as F
-from pyspark.sql import Row
-from pyspark.sql.functions import col, column, from_json, count, isnan, isnull, when, expr, to_timestamp, to_date, explode, flatten, lit, instr, hour, length, concat_ws, concat
-from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, ArrayType, MapType, IntegerType, BooleanType
-from pyspark.sql.functions import col, when, lit
-import json
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAI
-#from langchain.llms import OpenAI
 from pyspark.sql import SparkSession
-from langchain_experimental.agents import create_spark_dataframe_agent
-import requests
-import openai
-import spark
-import pyspark
+from pyspark.sql.functions import col
 import os
-from langchain_community.llms import OpenAI
-from pyspark.sql import SparkSession
-import pandas as pd
-from pyspark.sql import functions as F
-from pyspark.sql import Row
-from pyspark.sql.functions import col, column, from_json, count, isnan, isnull, when, expr, to_timestamp, to_date, explode, flatten, lit, instr, hour, length, concat_ws, concat
-from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, ArrayType, MapType, IntegerType, BooleanType
-from pyspark.sql.functions import col, when, lit
-import json
-from langchain.agents.agent_types import AgentType
-from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAI
-#from langchain.llms import OpenAI
-from pyspark.sql import SparkSession
-from langchain_experimental.agents import create_spark_dataframe_agent
-import requests
-import openai
-import spark
-import pyspark
-import os
-from langchain_community.llms import OpenAI
-from pyspark.sql import SparkSession
-from pyspark.sql import SQLContext
-from pyspark import SparkContext
-
 
 # OpenAI API Key
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 
 prompt = """You are an AI assistant that must read a csv and answer questions about it, 
 any question that I will say after this sentence is the one you must answer, and if at any 
