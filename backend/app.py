ee7589270bc85498030cb79c01989bc8f8da7cc3
Isa Khan <isakhan1000@gmail.com>
2024-05-12 14:39:08 -0400

    new app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-from LLM_requests import process_csv
+#from LLM_requests import process_csv
+from langchain_community.llms import OpenAI
 import pandas as pd
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import ChatOpenAI
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+prompt = """You are an AI assistant that must read a csv and answer questions about it, 
+any question that I will say after this sentence is the one you must answer, and if at any 
+point you are asked something that is not related to the csv or you do not understand, you 
+can simply reply with  "Sorry, I am unable to fulfill that request". Here is the question: 
+"""
+
+def process_csv(df, query):
+    # Your processing code here
+    agent = create_pandas_dataframe_agent(
+        ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
+        df,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+    )
+    result = agent.run(prompt+query)
+
+    return result 
 
 app = Flask(__name__, static_folder='../static', template_folder='templates')  
 
